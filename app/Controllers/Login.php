09372f2626eaf4210a93792c5cc2bac21b4847b1
Gusma <agussulistionoa0@gmail.com>
2024-01-10 15:46:33 +0700

    module login register

---
+++ app/Controllers/Login.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\UserModel;
+user \Firebase\
+class Login extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
